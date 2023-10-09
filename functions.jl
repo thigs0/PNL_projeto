e8f98c3e64b6e3e21ca33d8864f14a45d3659ff3
thigs0 <tthiagosantos38@gmai.com>
2023-10-09 11:07:31 -0300

    Newton

---
+++ functions.jl
@@ -0,0 +1,17 @@
+using LinearAlgebra
+
+function Newton(α::Int, β::Int, γ::Int, σ::Int, ρ::Int, ε::Int=10^-5, M::Int, x::Vector, f::Function)
+  #=
+  #
+  =#
+  k = 0;
+  ∇f = grad(x)
+  if (norm(∇f) >= ε) && (k < M)
+    μ = 0;
+    try
+      Hess = 1 #calculete hessian
+      t = Hess+Matrix(Float64, I(size(Hess)))
+  end
+
+
+end
