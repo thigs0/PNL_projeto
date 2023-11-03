c933af4c8c901bca20d0670b523057c9b4ad92bc
biajuliao <biajuliaoreis@gmail.com>
2023-11-03 00:17:41 -0300

    update - generalizar metodos dos testes

--- testes.jl
+++ testes.jl
@@ -3,30 +3,30 @@ include("metodos.jl")
 using TickTock
 using Plots
 
-function testarN(f::Function; n::Int=500, N::Int=30)
-    #Teste a função
-    #otimiza com método do gradiente
+function testarN(f::Function, Metodo::Function; n::Int=500, N::Int=30)
+    #Teste a função f
+    #otimizada com método Metodo
     tempo = ones(n)
     x = rand(N)
     y = ones(n)
     for i in 1:n
       start = time()
-      y[i] = f(Gradiente(x, f, M=i))
+      y[i] = f(Metodo(x, f, M=i))
       tempo[i] = time()-start
     end
     p = scatter(1:n, tempo);
-    println("A solução obtida pelo método do gradiente para função quadrática é:")
-    print(Gradiente(x, f, M=n))
+    println("A solução obtida pelo método $Metodo para função quadrática é:")
+    print(Metodo(x, f, M=n))
 
     return (p, y[n])
 end 
 
-function caminho(f::Function; n::Int=10, x::Vector{Float64}=rand(2))
+function caminho(f::Function, Metodo::Function; n::Int=10, x::Vector{Float64}=rand(2))
   #Desenha n primeiros pontos
   z = ones(n+1, 2)
   z[1,:] = x
   for i in 1:n
-    z[i+1,:] = Gradiente(x, f, M=i)
+    z[i+1,:] = Metodo(x, f, M=i)
   end
 
   eixo_x = Vector(range(minimum(z[:,1])-2, maximum(z[:,1])+2, 100))
